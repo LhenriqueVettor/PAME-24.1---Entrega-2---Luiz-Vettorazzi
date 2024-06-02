bcb3801434bcec5b25e949271589040f0263e725
LhenriqueVettor <lhenriquevettor.20241@poli.ufrj.br>
2024-06-02 00:02:05 -0300

    Arquivos PAME 2024.1 HTML - ENTREGA 2

---
+++ EntregaFluxo2/Entrega2Fluxo.js
@@ -0,0 +1,15 @@
+//Definir classes -x- Não feito ainda
+class Usuario {
+    constructor(Email,senha) {
+        this.Email = Email
+        this.senha = senha
+    }
+
+}
+class Sistema {
+    usuarios = []
+
+    FazerCadastro() {
+        const 
+    }
+}
